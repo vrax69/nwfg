ca522dcc6af6997b3c002f90726c261997b0fbbd
Brian <bllanes@newwavepower.net>
2025-02-04 16:09:38 -0500

    fondo rates, logo nwfg, layout estandar y svg aplicado

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -1,8 +1,14 @@
+import styles from "./rates.module.css"; // Importa los estilos de CSS Modules
+
 export default function RatesPage() {
     return (
-      <div className="text-2xl font-bold text-red-500">
-        Página de Tarifas
-      </div>
+        <div className={styles.container}>
+            {/* Logo con una CLASE en vez de un ID */}
+            <img 
+                src="https://rates-nwpc.s3.us-east-2.amazonaws.com/LOGO_NWFG-.png" 
+                alt="Logo NWFG" 
+                className={styles.logo} 
+            />
+        </div>
     );
-  }
-  
+}
