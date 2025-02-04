8383be75ad7fa5c673dac579a53433c3f547440b
Brian <bllanes@newwavepower.net>
2025-02-03 19:10:17 -0500

    cambios en /dashboard las tarjetas al fin pudieron ser posicionadas

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+import PixelCards from "../../components/ui/pixel-card"; // Importa correctamente el componente
 
 export default function DashboardPage() {
   return (
     <BackgroundBeamsWithCollision>
-      <div /> {/* Esto evitará el error */}
+      <div className="flex justify-center items-center h-screen w-full">
+        <PixelCards /> {/* Muestra las tarjetas directamente */}
+      </div>
     </BackgroundBeamsWithCollision>
   );
 }
-  
---
+++ global.d.ts
@@ -0,0 +1,6 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      "pixel-canvas": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+  
