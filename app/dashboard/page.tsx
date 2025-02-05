085918e1caba6e79f9d929aacb2107f73b5f1ac2
Brian <bllanes@newwavepower.net>
2025-02-05 17:50:59 -0500

    recuerda agrega export const metadata para cada pagina tenga su nombre

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import PixelCards from "../../components/ui/pixel-card"; // Importa correctamente el componente
 
+// Agrega esta configuración de metadata
+export const metadata = {
+  title: "Dashboard 📊",
+};
+
 export default function DashboardPage() {
   return (
     <BackgroundBeamsWithCollision>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "@/app/styles/globals.css"; // Asegúrate de importar estilos globales
 
 export const metadata: Metadata = {
   title: "Nwfg",
-  description: "Sistema de gestión y autenticación",
+  description: "ayuda a tus a gentes a hacer menos tiempo en la venta con estas multiples herramietnas, optimiza y encargate de",
   icons: {
     icon: "https://rates-nwpc.s3.us-east-2.amazonaws.com/rtrete.png",
   },
