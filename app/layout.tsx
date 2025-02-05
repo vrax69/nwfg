771dc0fd96b961cfdcfe8aedbb69787b11aabf10
Brian <bllanes@newwavepower.net>
2025-02-05 17:39:03 -0500

    se agrego el favicon https://rates-nwpc.s3.us-east-2.amazonaws.com/rtrete.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "@/app/styles/globals.css"; // Asegúrate de importar estilos globales
 export const metadata: Metadata = {
   title: "Nwfg",
   description: "Sistema de gestión y autenticación",
+  icons: {
+    icon: "https://rates-nwpc.s3.us-east-2.amazonaws.com/rtrete.png",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
