9d3b4a2965e7de70ed450fd01e46cfb581fea8e5
Brian <bllanes@newwavepower.net>
2025-02-06 09:32:14 -0500

    le pusimos el titles a /rate es importante exportar y luego return el contenido

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic"
 import styles from "./rates.module.css"
+import { useEffect } from "react"; // Importa useEffect
 
 // Dynamic import of the Input component
 const Input = dynamic(() => import("../../components/ui/Input"), {
@@ -10,6 +11,11 @@ const Input = dynamic(() => import("../../components/ui/Input"), {
 })
 
 export default function RatesPage() {
+   // Cambia el título de la pestaña usando useEffect
+   useEffect(() => {
+    document.title = "Rates Nwfg "; // Título personalizado
+  }, []);
+
   return (
     <div className={styles.container}>
       <img src="https://rates-nwpc.s3.us-east-2.amazonaws.com/LOGO_NWFG-.png" alt="Logo NWFG" className={styles.logo} />
