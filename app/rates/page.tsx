a94480cc3b6d6459e55ef26f720b6bc7b4c4c974
Brian <bllanes@newwavepower.net>
2025-02-07 15:09:17 -0500

    se añadieron card y los componentes input button card label para su uso

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -5,7 +5,7 @@ import styles from "./rates.module.css"
 import { useEffect } from "react"; // Importa useEffect
 
 // Dynamic import of the Input component
-const Input = dynamic(() => import("../../components/ui/Input"), {
+const Input_rates  = dynamic(() => import("../../components/ui/Input_rates"), {
   ssr: false,
   loading: () => <div>Loading...</div>,
 })
@@ -19,7 +19,7 @@ export default function RatesPage() {
   return (
     <div className={styles.container}>
       <img src="https://rates-nwpc.s3.us-east-2.amazonaws.com/LOGO_NWFG-.png" alt="Logo NWFG" className={styles.logo} />
-      <Input />
+      <Input_rates />
     </div>
   )
 }
