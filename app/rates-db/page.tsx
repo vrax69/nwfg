7fafa3becee79d8a5666621cf29836b67383ca03
Brian <bllanes@newwavepower.net>
2025-02-14 14:44:17 -0500

    se cambia el link por el nuevo nombre/icono

--- app/rates-db/page.tsx
+++ app/rates-db/page.tsx
@@ -103,8 +103,17 @@ const RatesDbPage = () => {
       onClick: () => setShowCard(!showCard)
     },
     {
-      title: "rates",
-      icon: <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-table-spark"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5" /><path d="M12 21h-7a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7" /><path d="M3 10h18" /><path d="M10 3v18" /></svg>,
+      title: "Rates",
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-table-export">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M12.5 21h-7.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5" />
+          <path d="M3 10h18" />
+          <path d="M10 3v18" />
+          <path d="M16 19h6" />
+          <path d="M19 16l3 3l-3 3" />
+        </svg>
+      ),
       href: "/ui"
     },
     {
