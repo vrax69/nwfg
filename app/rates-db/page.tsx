1d2e72b63da6440bdae76d1fc882bd3e740d5c50
Brian <bllanes@newwavepower.net>
2025-02-14 13:44:00 -0500

    se cambio el icono a rates para mostrar la lista

--- app/rates-db/page.tsx
+++ app/rates-db/page.tsx
@@ -103,8 +103,8 @@ const RatesDbPage = () => {
       onClick: () => setShowCard(!showCard)
     },
     {
-      title: "Aceternity UI",
-      icon: <Image src="https://assets.aceternity.com/logo-dark.png" width={20} height={20} alt="Aceternity Logo" />,
+      title: "rates",
+      icon: <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-table-spark"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5" /><path d="M12 21h-7a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7" /><path d="M3 10h18" /><path d="M10 3v18" /></svg>,
       href: "/ui"
     },
     {
