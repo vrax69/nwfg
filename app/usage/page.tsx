83bb2f7a7a4209c83120e04c27073179e70741bf
Brian <bllanes@newwavepower.net>
2025-02-20 17:10:28 -0500

    se a hecho el select column en usage para cuando este listo añadirlo rates-db

--- app/usage/page.tsx
+++ app/usage/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Stepper, { Step } from "../../components/ui/stepper";
+import ColumnSelector from '../../components/ui/ColumnSelector'; // Asegúrate de importar el componente nuevo
 
 export default function Page() {
   const [name, setName] = useState<string>(""); // Definir el estado correctamente
@@ -17,29 +18,17 @@ export default function Page() {
       nextButtonText="Next"
     >
       <Step>
-        <h2>Welcome to the React Bits stepper!</h2>
-        <p>Check out the next step!</p>
+        <h2>Bienvenido al stepper para cambiar las tarifas!</h2>
+        <p>Recuerda seguir cada paso al pie de la letra!</p>
       </Step>
       <Step>
-        <h2>Step 2</h2>
-        <img
-          style={{
-            height: "100px",
-            width: "100%",
-            objectFit: "cover",
-            objectPosition: "center -70px",
-            borderRadius: "15px",
-            marginTop: "1em",
-          }}
-          src="https://www.purrfectcatgifts.co.uk/cdn/shop/collections/Funny_Cat_Cards_640x640.png?v=1663150894"
-          alt="Funny Cat"
-        />
-        <p>Custom step content!</p>
+        <h2>Selecciona tus columnas</h2>
+        <ColumnSelector />
       </Step>
       <Step>
         <h2>How about an input?</h2>
         <input
-          value={name} // Estado definido correctamente
+          value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Your name?"
         />
