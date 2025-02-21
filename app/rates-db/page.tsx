1a9f86590cf9bb1e4dd269b10d09235b6cbf5924
Brian <bllanes@newwavepower.net>
2025-02-21 12:38:13 -0500

    se le integro el unuevo paso 2 al stepper en rates-db

--- app/rates-db/page.tsx
+++ app/rates-db/page.tsx
@@ -24,6 +24,7 @@ import {
 } from "@/components/ui/select";
 import { FileUpload } from "../../components/ui/file-upload";
 import Stepper, { Step } from "../../components/ui/stepper"; // Importamos el Stepper
+import ColumnSelector from '../../components/ui/ColumnSelector'; // Importamos el ColumnSelector
 
 interface CardWithFormProps {
   onCancel: () => void;
@@ -180,20 +181,8 @@ const RatesDbPage = () => {
                   <p>recuerda seguir cada paso al pie de la letra!</p>
                 </Step>
                 <Step>
-                  <h2>paso 2</h2>
-                  <img
-                    style={{
-                      height: "100px",
-                      width: "100%",
-                      objectFit: "cover",
-                      objectPosition: "center -70px",
-                      borderRadius: "15px",
-                      marginTop: "1em",
-                    }}
-                    src="https://www.purrfectcatgifts.co.uk/cdn/shop/collections/Funny_Cat_Cards_640x640.png?v=1663150894"
-                    alt="Funny Cat"
-                  />
-                  <p>Custom step content!</p>
+                  <h2>Selecciona tus columnas</h2>
+                  <ColumnSelector />
                 </Step>
                 <Step>
                   <h2>How about an input?</h2>
