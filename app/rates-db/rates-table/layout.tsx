a6c66beaf754802abed84ecc8e4b00caedcce3d2
brian <bllanes@newwavepower.net>
2025-03-10 11:52:13 -0500

    se añade el componente Layout para la gestión de metadatos en la base de datos de tarifas

---
+++ app/rates-db/rates-table/layout.tsx
@@ -0,0 +1,13 @@
+// app/rates-db/layout.tsx (Server Component)
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tables Data Base | Nwfg",
+  icons: {
+    icon: "https://rates-nwpc.s3.us-east-2.amazonaws.com/rtrete.png",
+  },
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
